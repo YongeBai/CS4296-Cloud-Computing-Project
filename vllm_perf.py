d4f89b992992c0bc313b286adedc384552057d98
Joseph Kim <clo3olb@gmail.com>
2024-04-16 04:37:58 +0000

    formatting changes

--- vllm_perf.py
+++ vllm_perf.py
@@ -4,6 +4,7 @@ from vllm.engine.async_llm_engine import AsyncLLMEngine
 from vllm.utils import random_uuid
 from timeit import default_timer as timer
 
+
 def ttft_measurer(prompt, args):
     llm = LLM(
         model=args.model,
@@ -11,22 +12,24 @@ def ttft_measurer(prompt, args):
         dtype=args.dtype,
     )
     tokenizer = llm.get_tokenizer()
+
     def single_request():
         sampling_params = SamplingParams(
-                temperature=0.0,
-                ignore_eos=True,
-                max_tokens=1,
-            )
+            temperature=0.0,
+            ignore_eos=True,
+            max_tokens=1,
+        )
         prompt_token_ids = tokenizer.encode(prompt)
         llm._add_request(
-                prompt=None,
-                prompt_token_ids=prompt_token_ids,
-                sampling_params=sampling_params,
-                )
+            prompt=None,
+            prompt_token_ids=prompt_token_ids,
+            sampling_params=sampling_params,
+        )
         start = timer()
         llm._run_engine(use_tqdm=False)
         return timer() - start
     return single_request
+
 
 def tpot_measurer(prompt, args):
     engineArgs = AsyncEngineArgs(args.model)
@@ -38,10 +41,10 @@ def tpot_measurer(prompt, args):
 
     async def single_request():
         sampling_params = SamplingParams(
-                temperature=0.0,
-                ignore_eos=True,
-                max_tokens=args.output_tokens,
-            )
+            temperature=0.0,
+            ignore_eos=True,
+            max_tokens=args.output_tokens,
+        )
         request_id = random_uuid()
         results_generator = llm.generate(prompt, sampling_params, request_id)
         i = 0
@@ -49,8 +52,9 @@ def tpot_measurer(prompt, args):
             if i == 0:
                 start = timer()
             i += 1
-        return (timer() - start) / (i - 1)
+        return (timer() - start) / (i)
     return single_request
+
 
 def static_batch_measurer(prompt, args):
     llm = LLM(
@@ -59,19 +63,20 @@ def static_batch_measurer(prompt, args):
         dtype=args.dtype,
     )
     tokenizer = llm.get_tokenizer()
+
     def single_request():
         sampling_params = SamplingParams(
-                temperature=0.0,
-                ignore_eos=True,
-                max_tokens=args.output_tokens,
-            )
+            temperature=0.0,
+            ignore_eos=True,
+            max_tokens=args.output_tokens,
+        )
         prompt_token_ids = tokenizer.encode(prompt)
         for _ in range(args.batch_size):
             llm._add_request(
                 prompt=None,
                 prompt_token_ids=prompt_token_ids,
                 sampling_params=sampling_params,
-                )
+            )
         start = timer()
         llm._run_engine(use_tqdm=True)
         total_time = timer() - start
@@ -79,15 +84,16 @@ def static_batch_measurer(prompt, args):
         return tokens_count / total_time
     return single_request
 
+
 def rate_throughput_measurer(prompt, args):
     llm = init_async_llm(args)
 
     async def single_request():
         sampling_params = SamplingParams(
-                temperature=0.0,
-                ignore_eos=True,
-                max_tokens=args.output_tokens,
-            )
+            temperature=0.0,
+            ignore_eos=True,
+            max_tokens=args.output_tokens,
+        )
         request_id = random_uuid()
         results_generator = llm.generate(prompt, sampling_params, request_id)
         async for _ in results_generator:
@@ -95,36 +101,43 @@ def rate_throughput_measurer(prompt, args):
         return args.output_tokens
     return single_request
 
+
 def sample_rate_throughput_measurer(args):
     llm = init_async_llm(args)
+
     async def single_request(sample):
         sampling_params = SamplingParams(
-                temperature=0.0,
-                ignore_eos=True,
-                max_tokens=sample["output_len"],
-            )
+            temperature=0.0,
+            ignore_eos=True,
+            max_tokens=sample["output_len"],
+        )
         request_id = random_uuid()
-        results_generator = llm.generate(sample["prompt"], sampling_params, request_id)
+        results_generator = llm.generate(
+            sample["prompt"], sampling_params, request_id)
         async for _ in results_generator:
             pass
         return sample["output_len"]
     return single_request
 
+
 def sample_output_rate_throughput_measurer(args):
     llm = init_async_llm(args)
+
     async def single_request(sample):
         sampling_params = SamplingParams(
-                top_k=args.top_k,
-                temperature=args.temperature,
-                max_tokens=4096,
-            )
+            top_k=args.top_k,
+            temperature=args.temperature,
+            max_tokens=4096,
+        )
         request_id = random_uuid()
-        results_generator = llm.generate(sample["prompt"], sampling_params, request_id)
+        results_generator = llm.generate(
+            sample["prompt"], sampling_params, request_id)
         i = 0
         async for _ in results_generator:
             i += 1
         return i
     return single_request
+
 
 def init_async_llm(args):
     engineArgs = AsyncEngineArgs(args.model)
