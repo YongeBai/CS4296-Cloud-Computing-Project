312ae25edc58328b756483b9df40c4ff59d55544
Yonge Bai <yongebai@gmail.com>
2024-04-16 06:28:31 +0000

    adding tokens per second test

--- utils.py
+++ utils.py
@@ -6,18 +6,18 @@ def get_prompts():
     prompts_files = [f for f in files if f.endswith(".txt")]
     prompts = []
     for file in prompts_files:
-        with open("prompts/" + file, "r") as f:
-            prompts.append(f.read())
+        with open(f"prompts/{file}", "r") as f:
+            prompts.append((file, f.read()))
     return prompts
 
-
-def run_test_n_times(test, n, test_name, framework):
-    file_name = f"results/{framework}_{test_name}_results"
-    with open(file_name+".txt", "w") as f:
+def run_test_n_times(test, n, test_name, framework, prompt_size):
+    file_name = f"{framework}_{test_name}_{prompt_size}"
+    with open(f"results/{framework}/{file_name}.txt", "w") as f:
         f.write(file_name+"\n")
         total = 0
         for i in range(n):
             value = test()
             total += value
             f.write(f"Iteration {i}: {value}\n")
-        f.write(f"Average: {total/n}")
+        f.write(f"Average: {total/n}\n")
+
